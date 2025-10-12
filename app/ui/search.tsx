0ceeda02a187feba424e1122061efa3a570e3cac
Abuaisha-7 <heyrubedru8086@gmail.com>
2025-10-12 11:28:07 +0300

    implemented search and pagination using URL search params and Next.js APIs

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,16 +1,23 @@
+
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
 
-  const searchParams = useSearchParams();
+  const searchParams = useSearchParams(); //Access the current URL query params it's Read only
   const pathname = usePathname();
   const { replace } = useRouter();
-  function handleSearch(term: string) {
-    const params = new URLSearchParams(searchParams);
-  
+
+// 👇 Create a debounced version of handleSearch
+  const handleSearch = useDebouncedCallback((term: string) => {
+
+    // console.log(`Searching... ${term}`);
+    const params = new URLSearchParams(searchParams); // Create an editable copy
+     params.set('page', '1'); // reset the page number to 1
+
     if (term) {
       params.set('query', term);
     } else {
@@ -19,7 +26,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
 
     replace(`${pathname}?${params.toString()}`);
 
-  }
+  }, 300); // 👈 runs after 300ms of no typing
+
 
 
   return (

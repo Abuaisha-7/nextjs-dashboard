d9680698904132891f68458ae271d210739953db
Abuaisha-7 <heyrubedru8086@gmail.com>
2025-10-12 17:38:35 +0300

    Server Actions to mutate data. and how to use the revalidatePath API to revalidate the Next.js cache and redirect to redirect the user to a new page.

---
+++ app/dashboard/invoices/create/page.tsx
@@ -0,0 +1,23 @@
+import Form from '@/app/ui/invoices/create-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchCustomers } from '@/app/lib/data';
+ 
+export default async function Page() {
+  const customers = await fetchCustomers();
+ 
+  return (
+    <main>
+      <Breadcrumbs //Breadcrumbs are a small navigation trail that shows users where they are in your app and helps them go back to previous pages easily.
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Create Invoice',
+            href: '/dashboard/invoices/create',
+            active: true,
+          },
+        ]}
+      />
+      <Form customers={customers} />
+    </main>
+  );
+}
